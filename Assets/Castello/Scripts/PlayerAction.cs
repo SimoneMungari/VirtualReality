dbe5fa5e59ad5d75bcae97265b9ac9dbc2521e2d
Simone Mungari <simone.mungari@revelis.eu>
2021-08-01 11:23:30 +0200

    added castle

---
+++ Assets/Castello/Scripts/PlayerAction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerAction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("f")) //pulsante sinistro
+        {
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(transform.position, Vector3.forward, out hit))
+            {
+                GameObject hitObject = hit.transform.gameObject;
+                Debug.Log(hitObject);
+                
+                if (hitObject.GetComponent<CastelloPortone>() != null && hit.distance < 5f)
+                {
+                    hitObject.GetComponent<CastelloPortone>().React();
+                }
+            }
+        }
+    }
+}
