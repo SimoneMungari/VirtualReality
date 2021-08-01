3a4431a76a643abedbbc86c56fb0156925f48468
Simone Mungari <simone.mungari@revelis.eu>
2021-08-01 16:06:19 +0200

    added castle

---
+++ Assets/Castello/Scripts/CastelloPortone.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CastelloPortone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void React()
+    {
+        Debug.Log("aaaa");
+        if (SceneManager.GetActiveScene().name == "scena")
+        {
+            SceneManager.LoadScene("Scenes/Castello");
+        }
+        else if (SceneManager.GetActiveScene().name == "Castello")
+        {
+            SceneManager.LoadScene("Scenes/scena");
+        }
+        
+    }
+}
